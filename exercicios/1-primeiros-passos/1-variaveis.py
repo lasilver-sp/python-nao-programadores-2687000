2b18bf725e261a23499e8121ac404c48c1f47042
lasilver-sp <162151090+lasilver-sp@users.noreply.github.com>
2024-03-04 16:10:59 +0000

    resposta do exercício

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,9 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+meu_nome = 'Luis André da Silveira'
 # Declare variáveis de cada tipo numérico
-
+minha_idade = "51"
 
 # Declare uma variável do tipo booleano
+Casado = True
